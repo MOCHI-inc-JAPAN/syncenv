07acc7c4bac0d00e51ddfcb33903c8d36a84fcca
tkow <t.kusama0613@outlook.jp>
2023-10-16 02:03:06 +0900

    fix: add plugin example and update readme

---
+++ example/custom-plugins/custom-plugin.ts
@@ -0,0 +1,26 @@
+import {Plugin, SyncenvConfig} from '@tkow/syncenv'
+
+export default class CustomPlugin extends Plugin {
+  static pluginId: "custom" = "custom";
+
+  constructor() {
+    super()
+  }
+
+  async fetchValues(replaces: Record<string, string>, config: SyncenvConfig): Promise<Record<string, string>> {
+    const results: Record<string, string> = {}
+    for(let [key, value] of Object.entries(replaces)) {
+      results[key] = 'pre-' + value
+    }
+    return results
+  }
+
+  loadPipes() {
+    return [
+      {
+        pipeId: 'postfix',
+        pipe: (value, id) => value + '-' + id
+      }
+    ]
+  }
+}
---
+++ example/custom-plugins/npm-plugin/npm-plugin.ts
@@ -0,0 +1,26 @@
+import {Plugin, SyncenvConfig} from '@tkow/syncenv'
+
+export default class CustomNpmPlugin extends Plugin {
+  static pluginId: "npm" = "npm";
+
+  constructor() {
+    super()
+  }
+
+  async fetchValues(replaces: Record<string, string>, config: SyncenvConfig): Promise<Record<string, string>> {
+    const results: Record<string, string> = {}
+    for(let [key, value] of Object.entries(replaces)) {
+      results[key] = 'npm-' + value
+    }
+    return results
+  }
+
+  loadPipes() {
+    return [
+      {
+        pipeId: 'npm-postfix',
+        pipe: (value, id) => value + '-npm'
+      }
+    ]
+  }
+}
